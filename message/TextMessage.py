2906b86920dd80bb48d508a87e7a8f93f99b8836
Your Name <you@example.com>
2022-04-02 14:02:15 +0800

    fix text message

--- message/TextMessage.py
+++ message/TextMessage.py
@@ -5,7 +5,8 @@ from .HTMLMessage import HTMLMessage
 @Registry.register_module
 class TextMessage(HTMLMessage):
     def __init__(self):
+        super(TextMessage, self).__init__()
         self.head = ""
         self.content_join = """\n"""
         self.tail = ""
-        super(TextMessage, self).__init__()
+
