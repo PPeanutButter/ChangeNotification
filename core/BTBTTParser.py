1179e6808af8402f2ec51362853bdefc8b4b0a34
Your Name <you@example.com>
2022-04-02 14:06:42 +0800

    add emoji on bt

--- core/BTBTTParser.py
+++ core/BTBTTParser.py
@@ -21,7 +21,7 @@ class BTBTTParser(CSSParser):
         if selected.name == 'a' and str(selected.text).endswith('.torrent'):
             download_url = f"{ori_url.scheme}://{ori_url.hostname}/{selected.attrs['href']}"\
                 .replace("-dialog-", "-download-")
-            return self.get_id(selected) + '\n'+os.popen(f'bt2magnet "{download_url}"').read().strip('\n')
+            return '📺'+self.get_id(selected) + '\n🧲'+os.popen(f'bt2magnet "{download_url}"').read().strip('\n')
         else:
             return self.get_id(selected)
 
